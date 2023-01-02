74b1877066ce4fcc4a9eb5087614a95e9d78780a
Rizkyrnk <115978912+Rizkyrnk@users.noreply.github.com>
2023-01-02 12:07:58 +0700

    menambahkan java dan koneksi java

---
+++ Koneksi.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sisteminformasiperpus;
+
+/**
+ *
+ * @author Fadil
+ */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+ 
+public class Koneksi {
+    Connection con;
+    Statement stm;
+    
+    public void config(){
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con=DriverManager.getConnection("jdbc:mysql://localhost/perpus", "root", "");
+            stm = con.createStatement();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "koneksi gagal "+e.getMessage());
+        }
+    }
+}
